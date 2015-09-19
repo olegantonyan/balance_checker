0af55fe28cd7e7530b759e971e3b2e3f3ef108f7
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-09-19 11:37:53 +0200

    Added email notification

--- lib/balance_checker/notifiers/email.rb
+++ lib/balance_checker/notifiers/email.rb
@@ -1,14 +1,34 @@
+require 'net/smtp'
+
 module Notifiers
   class Email
-    attr_reader :name
+    attr_reader :name, :config, :to, :from
 
     def initialize(options = {})
       @name = options.fetch(:name, nil)
       raise ArgumentError, "no config for notifier #{self.class}" unless @name
+      @config = options.fetch(:config)
+      @to = config[:to]
+      @from = config[:smtp][:user_name]
     end
 
     def deliver msg
-      puts "Notification: #{msg}"
+      message = <<-MESSAGE
+      From: #{from}
+      To: #{to}
+      Subject: #{msg}
+
+      #{msg}
+      MESSAGE
+
+      if config[:smtp]
+        puts "Sending email to #{to}"
+        smtp = Net::SMTP.new(config[:smtp][:address], config[:smtp][:port])
+        smtp.enable_starttls if config[:smtp][:enable_starttls_auto]
+        smtp.start(config[:smtp][:user_name].split('@')[1], config[:smtp][:user_name],  config[:smtp][:password],  config[:smtp][:authentication])
+        smtp.send_message(message, from, to)
+        smtp.finish
+      end
     end
   end
 end
